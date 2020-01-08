62dd4fac569fb86c13bf2602583e6a5ddfa31e36
Santiago Pena Sanabria <sapena@dnp.gov.co>
2020-01-08 16:23:42 -0500

    template ready to use

--- app/appl/__init__.py
+++ app/appl/__init__.py
@@ -9,7 +9,7 @@ mongo = PyMongo()
 def create_app():
     #initialize the app
     app = Flask(__name__, instance_relative_config=False)
-    app.config["MONGO_URI"] =  'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+    app.config["MONGO_URI"] =  'mongodb://' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
 
     #initialize plugins
     mongo.init_app(app = app)
